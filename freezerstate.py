5998ab17efc3c620349de3124c51f3a7d9967b15
Jeff Kelly <jgkelly6569@hotmail.com>
2020-04-30 21:40:40 -0400

    Created freezerstate.py file

---
+++ freezerstate.py
@@ -0,0 +1,18 @@
+"""
+freezerstate.py
+
+Temperature sensing and alerting application to be run on a Raspberry Pi. 
+
+Copyright (c) 2020, Jeff Kelly
+Licensed under the MIT license scheme
+"""
+
+import sys
+import time
+from datetime import datetime
+from os import listdir, system
+
+#from flask import Flask, jsonify
+#from flask.ext.cors import CORS
+
+# TODO: Code goes here. 
