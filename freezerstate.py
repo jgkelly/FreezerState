432f84a5c5ecf085d8c390de49ef1b20c8b62ddd
Jeff Kelly <jgkelly6569@hotmail.com>
2020-05-04 16:35:36 -0400

    Added sensor detection and reading

--- freezerstate.py
+++ freezerstate.py
@@ -3,6 +3,12 @@ freezerstate.py
 
 Temperature sensing and alerting application to be run on a Raspberry Pi. 
 
+Code based on ModMyPi's tutorial:
+- http://www.modmypi.com/blog/ds18b20-one-wire-digital-temperature-sensor-and-the-raspberry-pi
+
+And Tim Fernando's "temperature-serve-pi" project:
+- https://github.com/timfernando/temperature-serve-pi
+
 Copyright (c) 2020, Jeff Kelly
 Licensed under the MIT license scheme
 """
@@ -16,14 +22,44 @@ from os import listdir, system
 #from flask.ext.cors import CORS
 
 DEVICE_FOLDER = "/sys/bus/w1/devices/"
-DEVICE_SUFFIX = "/W1_slave"
-WAIT_INTERNAL = 0.2
+DEVICE_SUFFIX = "/w1_slave"
+#WAIT_INTERNAL = 0.2
 
 system('modprobe w1-gpio')
 system('modprobe w1-therm')
     
-def main():
+def raw_temperature():
+    f = open(find_sensor(), 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def find_sensor():
+    devices = listdir(DEVICE_FOLDER)
+    devices = [device for device in devices if device.startswith('28-')]
+    if devices:
+        return DEVICE_FOLDER + devices[0] + DEVICE_SUFFIX
+    else:
+        sys.exit("Could not find temperature sensor...")
+
+def get_temperature():
+    lines = raw_temperature()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = raw_temperature()
+    output_temperature = lines[1].find('t=')
+    if output_temperature != 1:
+        temperature_string = lines[1].strip()[output_temperature+2]
+        celsius = float(temperature_string) / 1000.0
+        farenheit = celsius * 9.0 / 5.0 + 32.0
+        return celsius, farenheit
     
+def main():
+    print ('Getting temperature...')
+    
+    while True:
+        print(f'Time: {datetime.now()} - {get_temperature()}\n')
+        time.sleep(1)
     return
 
 if __name__ == "__main__":
