954567547b80e254996e9789a3f6b7f3b456dd9a
Jeff Kelly <jgkelly6569@hotmail.com>
2020-05-14 14:22:22 -0400

    Added "uptime" text.

--- freezerstate/notify.py
+++ freezerstate/notify.py
@@ -65,7 +65,7 @@ class Notifier:
         uptime_diff = current_time - freezerstate.START_TIME
         uptime = uptime_diff.total_seconds() / 3600 
 
-        message = f'*{self.location}* status update.\n🌡 {freezerstate.CONVERSION.TemperatureString(temperature, True)}\n⏰ {timestring}\n💻 {uptime} hours'
+        message = f'*{self.location}* status update.\n🌡 {freezerstate.CONVERSION.TemperatureString(temperature, True)}\n⏰ {timestring}\n💻 Uptime: {uptime} hours'
         print(f'--- {current_time}: Sending uptime notification')
         self.last_notify = current_time
         self.send_all_notifiers(message)
